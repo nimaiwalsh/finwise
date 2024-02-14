0c83ca13a976df531c971e3d6114365f0f12da4d
Nimai Walsh <nimai.walsh@gmail.com>
2024-02-14 21:21:11 +1000

    Update androidx activity to support edge to edge

--- app/src/main/kotlin/com/finwise/android/MainActivity.kt
+++ app/src/main/kotlin/com/finwise/android/MainActivity.kt
@@ -3,11 +3,13 @@ package com.finwise.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.finwise.feature.home.HomeScreen
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent {
